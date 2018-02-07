0c8865a2bb3575ad42e7060cc689e02cf81690d4
Kristian Angelski <kristian.angelski@gmail.com>
2018-02-07 17:53:58 +0200

    express set to load public and src/views folders as default - now localhost starts with good-looking view

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var app = express();
+var port = 5000;
 
-var hi = 'hello';
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
-if (true) {
-    console.log('hi');
-}
+// Simple routing
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
 
-console.log(hi);
+app.get('/books', function(req, res) {
+    res.send('hello books');
+});
+
+app.listen(port, function(err) {
+    console.log('running server on port ' + port);
+});
